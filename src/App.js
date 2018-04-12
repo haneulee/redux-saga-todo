a4e8fc69891176d90317f66cfae0bcd377fafd72
haneulee <lovesky4294@gmail.com>
2018-04-12 18:59:25 +0900

    styled-components 적용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,43 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Button = styled.button`
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  background: white;
+`;
+
+const AsyncButton = Button.extend`
+  color: tomato;
+  border-color: tomato;
+`;
+
+const Input = styled.input`
+  padding: 0.5em;
+  margin: 0.5em;
+  color: palevioletred;
+  background: papayawhip;
+  border: none;
+  border-radius: 3px;
+`;
+
+const listStyle = {
+  listStyle: "none"
+};
 
 class App extends Component {
   constructor(props) {
@@ -27,21 +66,21 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <h1>TODO LIST</h1>
-        <input onChange={this.handleChange} value={this.state.inputVal} />
-        <button onClick={this.handleClick}>+</button>
+      <Wrapper>
+        <Title>TODO LIST</Title>
+        <Input onChange={this.handleChange} value={this.state.inputVal} />
+        <Button onClick={this.handleClick}>+</Button>
 
         <ul>
           {
             this.props.todoList.map(({ text, id }, index) => (
-              <li key={index}>{text}
-                <button id={id} onClick={this.handleClick}>-</button>
-                <button id={id} onClick={this.handleClick}>- (async)</button></li>
+              <li style={listStyle} key={index}>{text}
+                <Button id={id} onClick={this.handleClick}>-</Button>
+                <AsyncButton id={id} onClick={this.handleClick}>- (async)</AsyncButton></li>
             ))
           }
         </ul>
-      </div>
+      </Wrapper>
     );
   }
 }
